b8512706ee893a416d48bcba7cc2aecdf1a5db66
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-06-17 17:42:57 -0300

    expo init

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR'
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import AppLoading from 'expo-app-loading';
+import theme from './src/global/styles/theme';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import { Routes } from './src/routes';
+import {store, persistor } from './store';
+// import  Auth from './src/hooks/useAuthChange';
+import Auth from './src/components/Main/Auth';
+
 
 export default function App() {
+	
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style='auto'/>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Auth fontsLoaded={fontsLoaded}>
+          <ThemeProvider theme={theme}>
+            <Routes/>
+          </ThemeProvider>
+        </Auth>
+      </PersistGate>
+    </Provider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
